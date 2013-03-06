759690d2d5bce78a14565acd8440a32e50787f8d
Misha Conway <MishaAConway@gmail.com>
2013-03-06 03:56:25 -0800

    got rid of test line of code

--- lib/wavefront.rb
+++ lib/wavefront.rb
@@ -7,8 +7,6 @@ require "group"
 require "wavefront_object"
 require "wavefront_file"
 
-@f = "/Users/misha/Desktop/more stuff/misha_ruby_gems/wavefront/lib/correct_knight_subdivided.obj"
-
 module Wavefront
 
 
